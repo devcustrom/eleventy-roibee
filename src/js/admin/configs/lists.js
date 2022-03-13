ca2a2998c0e7cfc64b0e9d0594d727f23c99f207
Danador <krut76527@gmail.com>
2022-03-13 12:28:27 +0300

    Добавил таблички

--- src/js/admin/configs/lists.js
+++ src/js/admin/configs/lists.js
@@ -1,10 +1,10 @@
 const lists = {
-	"label": 'Списки',
+	"label": 'Данные',
 	"name": 'lists',
 	"files": [
 		{
 			"name": "index",
-			"label": "Index",
+			"label": "Списки",
 			"file": "src/_data/index.json",
 			"widget" : "list",
 			"fields": [
@@ -51,6 +51,54 @@ const lists = {
 							"name": "text",
 							"label": "Текст",
 							"widget": "markdown",
+						}
+					]
+				}
+			]
+		},
+		{
+			"name": "tables",
+			"label": "Таблицы",
+			"file": "src/_data/tables.json",
+			"widget" : "list",
+			"fields": [
+				{
+					"name": "priceList",
+					"label": "Прайс лист",
+					"widget" : "list",
+					"fields": [
+						{
+							"label": "Название",
+							"name": "title",
+							"widget": "string"
+						},
+						{
+							"name": "list",
+							"label": "Данные",
+							"widget": "list",
+							"fields": [
+								{
+									"label": "Услуга",
+									"name": "title",
+									"widget": "string"
+								},
+								{
+									"label": "Описание",
+									"name": "subtitle",
+									"widget": "string"
+								},
+								{
+									"label": "Цена",
+									"name": "price",
+									"widget": "string"
+								},
+								{
+									"label": "Ссылка",
+									"name": "link",
+									"widget": "string",
+									"required": false
+								}
+							]
 						}
 					]
 				}
