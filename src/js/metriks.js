ab6891b1f8027c60d3d5f4f41251c4b7acde164e
Alexey Khachaturyan <devcustrom@gmail.com>
2022-03-18 14:51:34 +0300

    Метрика и чат

---
+++ src/js/metriks.js
@@ -0,0 +1,18 @@
+window.addEventListener('DOMContentLoaded', () => {
+	(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+	m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+	(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+	ym(86852439, "init", {
+		clickmap:true,
+		trackLinks:true,
+		accurateTrackBounce:true,
+		webvisor:true
+	});
+
+	window.replainSettings = {
+		id: '81269575-79a9-4b89-b277-eb8fd4a93fa5'
+	};
+	setTimeout(() => {
+		(function(u){var s=document.createElement('script');s.type='text/javascript';s.async=true;s.src=u;var x=document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);})('https://widget.replain.cc/dist/client.js');
+	}, 6000);
+})
