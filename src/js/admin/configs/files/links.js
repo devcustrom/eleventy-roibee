6f04b9ab64389bae4826f028692f8c179fe95d79
Danador <krut76527@gmail.com>
2022-03-19 18:27:06 +0300

    поле ссылок

---
+++ src/js/admin/configs/files/links.js
@@ -0,0 +1,49 @@
+const links = {
+    "name": "links",
+    "label": "Ссылки",
+    "file": "src/_data/links.json",
+    "widget" : "list",
+    "fields": [
+        {
+            "name": "menuList",
+            "label": "Верхнее меню",
+            "widget" : "list",
+            "fields": [
+                {
+                    "label": "текст ссылки",
+                    "name": "anchor",
+                    "widget": "string"
+                },
+                {
+                    "label": "ссылка",
+                    "name": "link",
+                    "widget": "string",
+                }
+            ]
+        },
+        {
+            "name": "socNet",
+            "label": "Социальные сети",
+            "widget" : "list",
+            "fields": [
+                {
+                    "label": "Текст ссылки",
+                    "name": "anchor",
+                    "widget": "string"
+                },
+                {
+                    "label": "ссылка",
+                    "name": "linkText",
+                    "widget": "string",
+                },
+                {
+                    "label": "Имя иконки",
+                    "name": "icon",
+                    "widget": "string",
+                }
+            ]
+        }
+    ]
+}
+
+export { links }
