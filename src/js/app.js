15a10f34aaa4ab1c1e52b0c3b445439b4009cfda
Alexey Khachaturyan <devcustrom@gmail.com>
2022-03-20 21:17:33 +0300

    Сделал закрытие меню при клике вне меню

--- src/js/app.js
+++ src/js/app.js
@@ -3,11 +3,14 @@ import lozad from 'lozad'
 import Cookies from './tools/cookies'
 import ContactForm from '../_includes/partials/contact-us/form/form'
 import Services from '../_includes/partials/services/services';
+import Size from './tools/size';
 import './metriks'
 
 window.Alpine = Alpine
 
 window.Cookies = Cookies
+
+window.Size = Size
 
 Alpine.start()
 
