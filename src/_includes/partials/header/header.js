26495cc0bbd6198bc15e048898f18adaf46c67ac
Alexey Khachaturyan <devcustrom@gmail.com>
2022-03-21 12:06:52 +0300

    Поправи логику менюшки

---
+++ src/_includes/partials/header/header.js
@@ -0,0 +1,33 @@
+import Alpine from 'alpinejs'
+import Size from '/src/js/tools/size';
+
+export default new class Header {
+	constructor() {
+		this.handler();
+	}
+
+	header = () => ({
+		noTop: false,
+        h: 80,
+		show: false,
+		desktop: Size.checker(Size.md),
+		init() {
+			window.addEventListener('scroll', () => {
+				this.noTop = (document.body.scrollTop > this.h || document.documentElement.scrollTop > this.h);
+			})
+			Size.checker(Size.md, () => {
+				this.desktop = Size.checker(Size.md)
+			})
+		},
+		toggle() {
+			if(!this.desktop) {
+				this.bodyLock = !this.bodyLock;
+				this.show = !this.show
+			}
+		}
+	})
+
+	handler() {
+		Alpine.data('header', this.header)
+	}
+}
--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,14 @@
 import Alpine from 'alpinejs'
 import lozad from 'lozad'
 import Cookies from './tools/cookies'
-import ContactForm from '../_includes/partials/contact-us/form/form'
-import Services from '../_includes/partials/services/services';
-import Size from './tools/size';
+import ContactForm from '/src/_includes/partials/contact-us/form/form'
+import Services from '/src/_includes/partials/services/services';
 import './metriks'
+import Header from '/src/_includes/partials/header/header'
 
 window.Alpine = Alpine
 
 window.Cookies = Cookies
-
-window.Size = Size
 
 Alpine.start()
 
