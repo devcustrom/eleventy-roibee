2daa9543cbae50f058717db93c4b187cfb444a17
Alexey Khachaturyan <devcustrom@gmail.com>
2022-04-11 07:52:24 +0300

    Переписал форму под await

--- src/_includes/partials/contact-us/form/form.js
+++ src/_includes/partials/contact-us/form/form.js
@@ -33,32 +33,29 @@ export default new class ContactForm {
 				this.$refs.button.disabled = true
 			}
 		},
-		submit() {
+		async submit() {
 			this.buttonLabel = 'Отправка'
 			this.sending = true
-			const form = new FormData(this.$refs.form)
-
-			fetch(this.url, {
+			const form = {
 				method: 'POST',
 				mode: 'no-cors',
-				body: form
-			})
-				.then(() => {
-					this.message = 'Форма отправлена, ожидайте!<br> С вами свяжутся'
-				})
-				.catch(() => {
-					this.message = 'Увы, но произошла ошибка попробуйте еще раз!'
-				})
-				.finally(() => {
-					this.sent = true,
-					this.sending = false;
-					this.buttonLabel = 'Отправлено'
-					setTimeout(() => {
-						this.$refs.form.reset()
-						this.buttonLabel = 'Отправить',
-						this.sent = false
-					}, 15000)
-				})
+				body: new FormData(this.$refs.form)
+			}
+
+			try {
+				await fetch(this.url, form)
+				this.message = 'Форма отправлена, ожидайте!<br> С вами свяжутся'
+			} catch (e) {
+				this.message = 'Увы, но произошла ошибка попробуйте еще раз!'
+			} finally {
+				this.sent = true,
+				this.sending = false;
+				setTimeout(() => {
+					this.$refs.form.reset()
+					this.buttonLabel = 'Отправить',
+					this.sent = false
+				}, 15000)
+			}
 		}
 	})
 
