c5aab7cb0695514902a9d59b150b78d96a6f06d9
Alexey Khachaturyan <devcustrom@gmail.com>
2022-07-24 19:50:16 +0300

    Динамический импорт

--- js/app.js
+++ js/app.js
@@ -14,3 +14,11 @@ Alpine.start()
 
 const observer = lozad();
 observer.observe();
+
+// const focusmenu = document.querySelector('#focusmenu')
+// focusmenu.addEventListener('focusin', () => {
+// 	import('./test.js' /* webpackChunkName: "test" */).then(module => {
+// 		const initCarousel = module.default;
+// 		initCarousel.handler()
+// 	});
+// })
---
+++ js/test.js
@@ -0,0 +1,11 @@
+import focusLock from "dom-focus-lock";
+
+export default new class FocusMenu {
+	constructor() {
+		this.handler();
+	}
+
+	handler() {
+		console.log(focusLock);
+	}
+}
