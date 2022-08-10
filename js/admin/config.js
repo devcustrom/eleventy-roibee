79c3e6946f0b4896cf200edc6f2c466057c26c71
Iliya <krut76527@gmail.com>
2022-08-10 16:14:31 +0500

    правка по конфигу

--- js/admin/config.js
+++ js/admin/config.js
@@ -1,12 +1,13 @@
 import { blog } from '../admin/preview-templates/post.js'
 import { pages } from '../admin/preview-templates/page.js'
 import { lists } from '../admin/configs/lists.js'
-
+console.log(import.meta.env);
+console.log(import.meta.env.VITE_APP_BRANCH);
 const config = {
 	"backend": {
 		name: "github",
 		repo: "devcustrom/eleventy-roibee",
-		branch: process.env.APP_BRANCH
+		branch: import.meta.env.VITE_APP_BRANCH
 	},
 	load_config_file: false,
 	locale: "ru",
